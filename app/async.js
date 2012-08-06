231af2b27647d15ed860e54f98a63846de0b2143
Oscar Vargas <oscar.vargas@tangosource.com>
2012-08-06 16:30:40 -0500

    added new test

--- app/async.js
+++ app/async.js
@@ -7,7 +7,20 @@ define([ 'jquery' ], function($) {
     },
 
     manipulateRemoteData : function(url) {
-
+      return{
+      then: function(cb){
+        $.ajax({
+          url:url
+        }).done(function(data){
+          var users = [];
+          for(var i=0;i<= data.people.length-1; i++){
+            users.push(data.people[i].name); 
+          }
+            users.sort();
+          return cb(users); 
+        });
+      }
+      }
     }
   };
 });
